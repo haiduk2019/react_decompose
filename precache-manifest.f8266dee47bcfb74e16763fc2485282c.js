5ef6f100549f38bc91f2ba07a952d098038bbcb2
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-30 15:37:16 +0300

    Updates

---
+++ precache-manifest.f8266dee47bcfb74e16763fc2485282c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e9e75a433075e93c481957ab557486",
+    "url": "./index.html"
+  },
+  {
+    "revision": "be2806ca828afc507bf6",
+    "url": "./static/css/main.a0b1863e.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be2806ca828afc507bf6",
+    "url": "./static/js/main.2a0e4db4.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
